1d5c5b3f805cc8917fed4a7d2c9421ab450c9981
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-14 15:48:27 -0400

    Complete quicksort logic draft

--- javascript.ts
+++ javascript.ts
@@ -1,3 +1,31 @@
 const input = [1, 2, 1231, 1231, 22, 1231, 51829, 182, 192, 1001];
 
-const QuickSort = (input: number[]) => {};
+const QuickSort = (input: number[]) => {
+    // trivially sort input less than 2 items
+    if (input.length <= 2) {
+        return input.sort((a, b) => a - b);
+    }
+
+    // pick pivot, rearrange, then recursively sort any input longer than 2
+    const pivotIndex = Math.floor(input.length / 2);
+
+    // move all greater than pivot items behind pivot index
+    for (let index = 0; index < pivotIndex; index++) {
+        if (input[index] > input[pivotIndex]) {
+            input.splice(input[pivotIndex], 0, input[index]);
+            input.splice(input[index], 1);
+        }
+    }
+
+    // move all less than pivot items before pivot index
+    for (let index = pivotIndex + 1; index < input.length; index++) {
+        if (input[index] < input[pivotIndex]) {
+            input.splice(0, 0, input[index]);
+            input.splice(input[index], 1);
+        }
+    }
+
+    return input
+        .slice(0, pivotIndex)
+        .concat([input[pivotIndex]], input.slice(pivotIndex + 1));
+};
