035d845bb82a27b7337861c2dab8cd2e0b96d518
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-14 16:09:54 -0400

    Update quicksort logic

--- javascript.ts
+++ javascript.ts
@@ -1,4 +1,4 @@
-const input = [1, 2, 1231, 1231, 22, 1231, 51829, 182, 192, 1001];
+const input = [12, 212, 5123, 3231, 22, 1231, 51829, 182, 192, 1001];
 
 const QuickSort = (input: number[]): number[] => {
     // trivially sort input less than 2 items
@@ -7,29 +7,31 @@ const QuickSort = (input: number[]): number[] => {
     }
 
     // pick pivot, rearrange, then recursively sort any input longer than 2
-    const pivotIndex = Math.floor(input.length / 2);
+    let pivotIndex = Math.floor(input.length / 2);
+    const pivot = input[pivotIndex];
 
     // move all greater than pivot items behind pivot index
     for (let index = 0; index < pivotIndex; index++) {
-        if (input[index] > input[pivotIndex]) {
-            input.splice(input[pivotIndex], 0, input[index]);
-            input.splice(input[index], 1);
+        if (input[index] > pivot) {
+            input.splice(pivotIndex, 0, input[index]);
+            input.splice(index, 1);
+            pivotIndex -= 1;
         }
     }
 
     // move all less than pivot items before pivot index
     for (let index = pivotIndex + 1; index < input.length; index++) {
-        if (input[index] < input[pivotIndex]) {
+        if (input[index] < pivot) {
             input.splice(0, 0, input[index]);
-            input.splice(input[index], 1);
+            input.splice(index, 1);
+            pivotIndex += 1;
         }
     }
-
     return QuickSort(input.slice(0, pivotIndex)).concat(
-        [input[pivotIndex]],
+        [pivot],
         QuickSort(input.slice(pivotIndex + 1))
     );
 };
 
 QuickSort(input);
-console.log(input);
+// console.log(input);
