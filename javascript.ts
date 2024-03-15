3ce8ac3712293c309c2f94623f1af3a586b4211d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-14 22:37:58 -0400

    Add improvement to empty left/right cases

--- javascript.ts
+++ javascript.ts
@@ -1,4 +1,4 @@
-let input = [12, 212, 5123, 3231, 22, 1231, 51829, 182, 192, 1001];
+let input = Array.from(Array(100), () => Math.floor(Math.random() * 100));
 
 const QuickSort = (input: number[]): number[] => {
     // trivially sort input less than 2 items
@@ -22,15 +22,47 @@ const QuickSort = (input: number[]): number[] => {
         }
     }
 
-    if (pivotIndex >= input.length || pivotIndex + 1 >= input.length) {
-        return input;
+    if (pivotIndex + 1 >= input.length) {
+        if (pivotIndex <= 0) {
+            return [pivot];
+        } else {
+            return QuickSort(input.slice(0, pivotIndex)).concat([pivot]);
+        }
+    } else if (pivotIndex <= 0) {
+        if (pivotIndex + 1 >= input.length) {
+            return [pivot];
+        } else {
+            return [pivot].concat(QuickSort(input.slice(0, pivotIndex)));
+        }
+    } else {
+        return QuickSort(input.slice(0, pivotIndex)).concat(
+            [pivot],
+            QuickSort(input.slice(pivotIndex + 1))
+        );
     }
-
-    return QuickSort(input.slice(0, pivotIndex)).concat(
-        [pivot],
-        QuickSort(input.slice(pivotIndex + 1))
-    );
 };
+
+function quicksort2(arr: number[]): number[] {
+    if (arr.length < 2) {
+        return arr;
+    }
+    const pivot = arr[Math.floor(Math.random() * arr.length)];
+
+    let left: number[] = [];
+    let right: number[] = [];
+    let equal: number[] = [];
+
+    for (let val of arr) {
+        if (val < pivot) {
+            left.push(val);
+        } else if (val > pivot) {
+            right.push(val);
+        } else {
+            equal.push(val);
+        }
+    }
+    return [...quicksort2(left), ...equal, ...quicksort2(right)];
+}
 
 console.log(input);
 input = QuickSort(input);
